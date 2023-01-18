56e7cac961a91a682058a79b9493f53e539ffa18
Jesus Pacheco <1104xyz@gmail.com>
2023-01-18 08:18:56 -0400

    enabling jsx eslint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -7,7 +7,10 @@ module.exports = {
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   plugins: ['react', 'prettier'],
   rules: {
