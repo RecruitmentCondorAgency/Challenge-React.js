37b47125c65c730f7d794c55f5a6a89906556962
Jesus Pacheco <1104xyz@gmail.com>
2023-01-18 09:41:11 -0400

    creating router boilerplate

--- web/index.js
+++ web/index.js
@@ -1,5 +1,34 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './app';
+import { App } from './src/app';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './src/pages/ErrorPage';
+import LoginPage from './src/pages/login/LoginPage';
+import PublicLayout from './src/layouts/Public';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/login',
+    element: <PublicLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/login',
+        element: <LoginPage />
+      }
+    ]
+  }
+]);
 
 const app = document.getElementById('app');
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+  app
+);
