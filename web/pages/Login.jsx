27a472d61e3f77d99af3df6f2a15037adaeea656
crisleo94 <cristhian.reinoso@live.com>
2023-01-26 17:06:41 -0500

    feat:create constant file to store global vars, finish login and register with formik validation, send POST request after a user is registered, show a simple toast after 201 response.status

--- web/pages/Login.jsx
+++ web/pages/Login.jsx
@@ -1,10 +1,39 @@
+import { useFormik } from 'formik'
 import { Button } from 'primereact/button'
 import { Checkbox } from 'primereact/checkbox'
 import { InputText } from 'primereact/inputtext'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const validate = values => {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = 'Required'
+  } else if (values.password.length < 8) {
+    errors.password = 'Must be 8 characters or more'
+  }
+
+  return errors
+}
 
 export default function Login() {
   const [checked, setChecked] = React.useState(false)
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    }
+  })
   return (
 
     <div className="flex align-items-center justify-content-center">
@@ -13,15 +42,33 @@ export default function Login() {
           <img src="https://cdn3.iconfinder.com/data/icons/strokeline/128/revisi_02-256.png" alt="hyper" height={50} className="mb-3" />
           <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
           <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-          <a href='' className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
+          <Link to='/register' className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</Link>
         </div>
 
-        <div>
+        <form onSubmit={formik.handleSubmit}>
           <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
-          <InputText id="email" type="text" placeholder="Email address" className="w-full mb-3" />
+          <InputText
+            id="email"
+            type="text"
+            placeholder="Email address"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            className="w-full"
+          />
+          {formik.touched.email && formik.errors.email ? <div className="text-red-500 text-xs">{formik.errors.email}</div> : null}
 
-          <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
-          <InputText id="password" type="password" placeholder="Password" className="w-full mb-3" />
+          <label htmlFor="password" className="block text-900 font-medium mb-2 mt-3">Password</label>
+          <InputText
+            id="password"
+            type="password"
+            placeholder="Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            className="w-full mb-3"
+          />
+          {formik.touched.password && formik.errors.password ? <div className="text-red-500 text-xs">{formik.errors.password}</div> : null}
 
           <div className="flex align-items-center justify-content-between mb-6">
             <div className="flex align-items-center">
@@ -31,8 +78,15 @@ export default function Login() {
             <a href='' className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
           </div>
 
-          <Button label="Sign In" icon="pi pi-arrow-right" iconPos='right' className="w-full" />
-        </div>
+          <Button
+            label="Sign In"
+            icon="pi pi-arrow-right"
+            iconPos='right'
+            className="w-full"
+            type='submit'
+            disabled={!(formik.isValid && formik.dirty)}
+          />
+        </form>
       </div>
     </div>
 
--- web/pages/Register.jsx
+++ web/pages/Register.jsx
@@ -1,9 +1,125 @@
+import { useFormik } from 'formik'
+import { Button } from 'primereact/button'
+import { InputText } from 'primereact/inputtext'
+import { Toast } from 'primereact/toast'
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { API_URL } from '../constants'
 
-function Register() {
-  return (
-    <div>Register</div>
-  )
+const validate = values => {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = 'Required'
+  } else if (values.password.length < 8) {
+    errors.password = 'Must be 8 characters or more'
+  }
+
+  if (values.confirmPassword !== values.password) {
+    errors.confirmPassword = 'Passwords do not match'
+  }
+
+  return errors
 }
 
-export default Register
+export default function Login() {
+  const toast = React.useRef(null)
+  let navigate = useNavigate()
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    },
+    validate,
+    onSubmit: values => {
+      const { email, password } = values
+      fetch(`${API_URL}/users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      }).then(res => {
+        if (res.status === 201) {
+          toast.current.show({ severity: 'success', summary: 'Success', detail: 'User created!', life: 3000 });
+
+          setTimeout(() => {
+            navigate('/login')
+          }, 2500)
+
+        }
+      }).catch(err => {
+        toast.current.show({ severity: 'error', summary: 'Error', detail: 'Server error', life: 3000 });
+      })
+    }
+  })
+  return (
+
+    <div className="flex align-items-center justify-content-center">
+      <Toast ref={toast} />
+      <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+        <div className="text-center mb-5">
+          <img src="https://cdn4.iconfinder.com/data/icons/ui-marketplace-1-0-flat/19/4_login-256.png" alt="hyper" height={50} className="mb-3" />
+          <div className="text-900 text-3xl font-medium mb-3">Welcome</div>
+          <span className="text-600 font-medium line-height-3">Already have an account?</span>
+          <Link to='/login' className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Login Here!</Link>
+        </div>
+
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
+          <InputText
+            id="email"
+            type="text"
+            placeholder="Email address"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            className="w-full"
+          />
+          {formik.touched.email && formik.errors.email ? <div className="text-red-500 text-xs">{formik.errors.email}</div> : null}
+
+          <label htmlFor="password" className="block text-900 font-medium mb-2 mt-3">Password</label>
+          <InputText
+            id="password"
+            type="password"
+            placeholder="Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            className="w-full"
+          />
+          {formik.touched.password && formik.errors.password ? <div className="text-red-500 text-xs">{formik.errors.password}</div> : null}
+
+          <label htmlFor="confirmPassword" className="block text-900 font-medium mb-2 mt-3">Confirm Password</label>
+          <InputText
+            id="confirmPassword"
+            type="password"
+            placeholder="Confirm Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.confirmPassword}
+            className="w-full"
+          />
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? <div className="text-red-500 text-xs">{formik.errors.confirmPassword}</div> : null}
+
+          <Button
+            label="Sign In"
+            icon="pi pi-arrow-right"
+            iconPos='right'
+            className="w-full mt-6"
+            type='submit'
+            disabled={!(formik.isValid && formik.dirty)}
+          />
+        </form>
+      </div>
+    </div>
+
+  )
+}
