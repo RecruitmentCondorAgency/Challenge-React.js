a83129064fa4fb06a462cc9153bc7682bae3fce0
Mario Marquez <46325641+ma-g-ma@users.noreply.github.com>
2023-02-01 00:30:15 -0600

    Add routing.

--- web/app.js
+++ web/app.js
@@ -1,6 +1,21 @@
 import './styles.css'
-import Header from "./components/UI/Header";
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import Login from "./pages/Login";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Layout from "./components/layout/Layout";
 
 export default function App() {
-	return <Header/>
+	return (
+		<Layout>
+			<Routes>
+				<Route path='/' element={<Login />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/search' element={<Search />} />
+				<Route path='/profile' element={<Profile />}/>
+				<Route path='*' element={<Login />} />
+			</Routes>
+		</Layout>
+	)
 }
