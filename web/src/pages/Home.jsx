91779bcea96ece29e487c662c32f9f61fca6b1fd
Joel Vizcarra <joelvizcarra98@gmail.com>
2023-03-15 15:11:50 -0500

    fixed home autocomplete spacing

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -142,7 +142,7 @@ const Home = () => {
           <Table {...weatherTableData} />
         </React.Fragment>
       )}
-      <Grid container spacing={2}>
+      <Grid container>
         <Grid item xs={12} md={4}>
           <AutocompleteInput
             options={countries}
