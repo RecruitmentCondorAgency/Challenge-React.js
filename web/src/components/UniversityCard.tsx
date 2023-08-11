7899fe1be383f1e86f164da00a7ff413e46fab85
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-08-11 13:51:30 -0600

    feat: add UniversityCard component

---
+++ web/src/components/UniversityCard.tsx
@@ -0,0 +1,25 @@
+type ResultProps = {
+  title: string;
+  country: string;
+  description: string;
+};
+
+export const UniversityCard: React.FC<ResultProps> = ({
+  title,
+  country,
+  description,
+}) => {
+  return (
+    <article className='drop-shadow-lg bg-white rounded-sm p-4'>
+      <div className='flex flex-col mb-6'>
+        <h3 className='text-black font-bold'>{title}</h3>
+        <span>{country}</span>
+        <div className='ml-auto flex gap-4'>
+          <button>X</button>
+          <button>O</button>
+        </div>
+      </div>
+      <p>{description}</p>
+    </article>
+  );
+};
