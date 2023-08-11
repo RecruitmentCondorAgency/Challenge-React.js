3f1fb3eee386c071af84450cdf83d43991cf48dc
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-08-11 10:19:57 -0600

    feat: add login prototype and add logo to web folder

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,48 @@
 import './styles.css';
-
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <header className='flex px-10 py-8 drop-shadow-lg bg-white mb-5'>
+        <img src='./logo.png' />{' '}
+        <a href='#' className='ml-auto'>
+          Search
+        </a>
+      </header>
+      <div className='bg-white max-w-lg mx-auto px-6 py-4 drop-shadow-lg rounded-md'>
+        <form action=''>
+          <fieldset className='flex flex-col gap-2 mb-4'>
+            <label htmlFor='user' className='after:content-["*"] after:ml-0.5'>
+              User
+            </label>
+            <input
+              className='p-2 border rounded-md'
+              placeholder='username'
+              type='text'
+              name='user'
+              id='user'
+            />
+            <label
+              htmlFor='password'
+              className='after:content-["*"] after:ml-0.5'
+            >
+              Password
+            </label>
+            <input
+              className='p-2 border rounded-md'
+              placeholder='****'
+              type='password'
+              name='password'
+              id='password'
+            />
+          </fieldset>
+          <button
+            className='bg-sky-500 px-6 py-2 rounded-md w-full text-white font-bold'
+            type='submit'
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
