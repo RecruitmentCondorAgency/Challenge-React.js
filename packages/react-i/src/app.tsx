7598e9b2a1bb5e527f3b13bbc320d7eb5f8a6a9d
webdesk <dev@webdesksolution.com>
2023-11-22 16:05:29 +0530

    submisstion dark theme light theme

--- packages/react-i/src/app.tsx
+++ packages/react-i/src/app.tsx
@@ -1,4 +1,29 @@
-import { Readme } from '../../../common/readme'
-import ReadmeB from '../README.mdx'
+import { Readme } from "../../../common/readme";
+import { ThemeContext } from "./contexts/theme-context";
+import { useState } from "react";
 
-export const App = () => <Readme readme={ReadmeB} />
+import ReadmeB from "../README.mdx";
+import Layout from "./layout";
+
+export const App = () => {
+  const isBrowserDefaulDark = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const getDefaultTheme = (): string => {
+    const localStorageTheme = localStorage.getItem("default-theme");
+    const browserDefault = isBrowserDefaulDark() ? "dark" : "light";
+    return localStorageTheme || browserDefault;
+  };
+  const [theme, setTheme] = useState(getDefaultTheme());
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`theme-${theme}`}>
+        <Layout>
+          <div className='content-wrapper'>
+            <Readme readme={ReadmeB} />
+          </div>
+        </Layout>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
---
+++ packages/react-i/src/layout/header.tsx
@@ -0,0 +1,44 @@
+import { FC, useContext } from "react";
+import { ThemeContext } from "../contexts/theme-context";
+
+import logoIcon from "../assets/logo.png";
+import "./layout.scss";
+
+const Header: FC = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const handleThemeChange = () => {
+    console.log(theme, "theme...");
+    const isCurrentDark = theme === "dark";
+    setTheme(isCurrentDark ? "light" : "dark");
+    localStorage.setItem("default-theme", isCurrentDark ? "light" : "dark");
+  };
+
+  return (
+    <header className='header'>
+      <div className='header-content'>
+        <a href='/' className='logo-section'>
+          <img src={logoIcon} alt='logo' />
+          <span>React App</span>
+        </a>
+        <div className='toggle-btn-section'>
+          <div className={`toggle-checkbox m-vertical-auto`}>
+            <input
+              className='toggle-btn__input'
+              type='checkbox'
+              name='checkbox'
+              onChange={handleThemeChange}
+              checked={theme === "light"}
+            />
+            <button
+              type='button'
+              className={`toggle-btn__input-label`}
+              onClick={handleThemeChange}></button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
