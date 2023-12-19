31fd72203f37c5c17149b0c1afc2a00bff82957d
Rafael González <reggon81@gmail.com>
2023-12-18 20:42:25 -0400

    fix(repo): update start script functionality

--- start-workspace.js
+++ start-workspace.js
@@ -1,13 +1,17 @@
 const prompt = require('prompt-sync')()
+let [workspaceName] = process.argv.slice(2)
 
-const workspaceName = prompt('Please enter the workspace name: ')
-
-if (workspaceName) {
-	const execSync = require('child_process').execSync
-	const command = `npm run start --workspace=${workspaceName}`
-
-	execSync(command, { stdio: 'inherit' })
-} else {
-	console.log('No workspace name provided. Exiting...')
-	process.exit(1)
+if (!workspaceName) {
+  workspaceName = prompt('Please enter the workspace name: ')
 }
+
+if (!workspaceName) {
+  console.log('No workspace name provided. Exiting...')
+  process.exit(1)
+}
+
+const execSync = require('child_process').execSync
+console.log(`Running ${workspaceName}`)
+const command = `npm run start --workspace=${workspaceName}`
+
+execSync(command, { stdio: 'inherit' })
