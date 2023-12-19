d4641cfd40d99edc2846e52abad0d116226943d2
Rafael González <reggon81@gmail.com>
2023-12-18 20:42:00 -0400

    fix(repo):fix tailwind warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
-
-	theme: {
-		extend: {}
-	},
-	plugins: []
+export default {
+  content: ['./packages/**/*.{html,js,ts,jsx,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
 }
